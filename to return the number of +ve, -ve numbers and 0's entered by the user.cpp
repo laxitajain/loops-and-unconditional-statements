ed46b822c77d780368d84432b68eb998e3df0d4f
laxita <87566518+laxitajain@users.noreply.github.com>
2023-11-29 11:31:13 +0530

    Update to return the number of +ve, -ve numbers and 0's entered by the user.cpp
    efficient use of the "toupper" function

--- to return the number of +ve, -ve numbers and 0's entered by the user.cpp
+++ to return the number of +ve, -ve numbers and 0's entered by the user.cpp
@@ -17,7 +17,7 @@ int main()
   do
    {cout<<"Do you wish to enter another number?(y/n) ";
    cin>>choice;
-   if(choice==toupper('y') || choice=='y')
+   if(topper(choice)=='Y')
      {cin>>x1;
      if(x1>0)
         p++;
